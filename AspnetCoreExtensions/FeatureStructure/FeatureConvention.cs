634347dcffe1872184e7d8027fbdfde75ca31f53
Riad Katby <morikapt@gmail.com>
2020-12-27 00:09:50 +0300

    Add FeatureStructure need classes Add WebExtensions class

---
+++ AspnetCoreExtensions/FeatureStructure/FeatureConvention.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AspnetCoreExtensions.FeatureStructure
+{
+    public class FeatureConvention : IControllerModelConvention
+    {
+        public void Apply(ControllerModel controller)
+        {
+            var featureName = GetFeatureName(controller.ControllerType);
+            controller.Properties.Add("feature", featureName);
+        }
+
+        private string GetFeatureName(TypeInfo controllerType)
+        {
+            string[] tokens = controllerType.FullName.Split('.');
+            if (!tokens.Any(t => t == "Features"))
+                return "";
+
+            string featureName = tokens
+                .SkipWhile(t => !t.Equals("features", StringComparison.CurrentCultureIgnoreCase))
+                .Skip(1)
+                .Take(1)
+                .FirstOrDefault();
+
+            return featureName;
+        }
+    }
+}
