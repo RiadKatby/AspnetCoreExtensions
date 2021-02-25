e2f492761aff71245ff6417d06bb02951982d1be
Mohammed R. Katby <mkatby@thiqah.sa>
2021-02-25 16:31:08 +0300

    add enum and stringBuilder extensions

---
+++ AspnetCoreExtensions/EnumExtensions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AspnetCoreExtensions
+{
+    public static class EnumExtensions<T>
+    {
+        public static IEnumerable<Tuple<string, string>> GetLookup()
+        {
+            var list = GetNames().Zip(GetDisplayValues())
+                .Select(x => new Tuple<string, string>(x.First, x.Second));
+
+            return list;
+        }
+
+        public static IList<T> GetValues()
+        {
+            var enumValues = new List<T>();
+
+            foreach (FieldInfo fi in typeof(T).GetType().GetFields(BindingFlags.Static | BindingFlags.Public))
+            {
+                enumValues.Add((T)Enum.Parse(typeof(T), fi.Name, false));
+            }
+            return enumValues;
+        }
+
+        public static T Parse(string value)
+        {
+            return (T)Enum.Parse(typeof(T), value, true);
+        }
+
+        public static IList<string> GetNames()
+        {
+            return typeof(T).GetFields(BindingFlags.Static | BindingFlags.Public).Select(fi => fi.Name).ToList();
+        }
+
+        public static IList<string> GetDisplayValues()
+        {
+            return GetNames().Select(obj => GetDisplayValue(Parse(obj))).ToList();
+        }
+
+        private static string lookupResource(Type resourceManagerProvider, string resourceKey)
+        {
+            foreach (PropertyInfo staticProperty in resourceManagerProvider.GetProperties(BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public))
+            {
+                if (staticProperty.PropertyType == typeof(System.Resources.ResourceManager))
+                {
+                    System.Resources.ResourceManager resourceManager = (System.Resources.ResourceManager)staticProperty.GetValue(null, null);
+                    return resourceManager.GetString(resourceKey);
+                }
+            }
+
+            return resourceKey; // Fallback with the key name
+        }
+
+        public static string GetDisplayValue(T value)
+        {
+            var fieldInfo = value.GetType().GetField(value.ToString());
+
+            var descriptionAttributes = fieldInfo.GetCustomAttributes(
+                typeof(DisplayAttribute), false) as DisplayAttribute[];
+
+            if (descriptionAttributes[0].ResourceType != null)
+                return lookupResource(descriptionAttributes[0].ResourceType, descriptionAttributes[0].Name);
+
+            if (descriptionAttributes == null) return string.Empty;
+            return (descriptionAttributes.Length > 0) ? descriptionAttributes[0].Name : value.ToString();
+        }
+    }
+}
---
+++ AspnetCoreExtensions/StringBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspnetCoreExtensions
+{
+    public static class StringBuilderExtensions
+    {
+        public static StringBuilder AppendIf(this StringBuilder sb, bool condition, string value)
+        {
+            if (condition)
+                sb.Append(value);
+
+            return sb;
+        }
+    }
+}
